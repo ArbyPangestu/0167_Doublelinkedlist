4494bbac83c2d7822e1faa8385849dab4087de93
ArbyPangestu <arbypangestu340@gmail.com>
2025-05-15 10:59:31 +0700

    menambahkan logika penghapusan node

--- doublelinkedist.cpp
+++ doublelinkedist.cpp
@@ -82,5 +82,13 @@ public:
             current->next = newNode; //Step 9d: current.next =newNode
     }
 
-  
-}
+    void hapus()
+    {
+        if (START = NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+
+        
+    }
